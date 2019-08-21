0915005fed7cebe820b1cb0bcafa00c9a3040222
Rusdi Haizim <rusdihaizim@gmail.com>
2019-08-21 18:30:41 +0800

    Completed Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import static java.nio.CharBuffer.wrap;
 
 public class Duke {
-
+    protected static List<String> dataList = new ArrayList<String>();
     private static String border = "____________________________________________________________";
     private static String logo =
               " ____        _        \n"
@@ -11,6 +13,7 @@ public class Duke {
             + "| | | | | | | |/ / _ \\\n"
             + "| |_| | |_| |   <  __/\n"
             + "|____/ \\__,_|_|\\_\\___|\n";
+    private static String input;
 
     /**
      * Program starts here.
@@ -26,19 +29,43 @@ public class Duke {
 
     private static void handleCommand() {
         Scanner sc = new Scanner(System.in);
-        level_1(sc);
+        //level_1(sc);
+        level_2(sc);
     }
 
     private static void level_1(Scanner sc) {
-        String input = "";
         while (sc.hasNext()) {
             input = sc.nextLine();
             if (input.equals("bye")) {
                 System.out.println(border + "\n" + wrap("Bye. Hope to see you again soon!") + "\n" + border + "\n");
                 break;
-            }
-            else {
+            } else {
                 System.out.println(border + "\n" + wrap(input) + "\n" + border + "\n");
+            }
+        }
+    }
+
+    private static void level_2(Scanner sc) {
+        while (sc.hasNext()) {
+            input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(border + "\n" + wrap("Bye. Hope to see you again soon!") + "\n" + border + "\n");
+                break;
+            } else if (input.equals("list")) {
+                int idx = 1;
+                System.out.println(border);
+                if (dataList.size() > 0) {
+                    for (String task : dataList) {
+                        System.out.print(idx++ + ". ");
+                        System.out.print(task + "\n");
+                    }
+                } else {
+                    System.out.println("Empty List!");
+                }
+                System.out.println(border);
+            } else {
+                dataList.add(input);
+                System.out.println(border + "\nadded: " + input + "\n" + border);
             }
         }
     }
