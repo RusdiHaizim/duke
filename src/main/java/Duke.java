5378a22c312ed2bac4e06e6918bb72f32ae8505c
Rusdi Haizim <rusdihaizim@gmail.com>
2019-08-21 18:00:04 +0800

    Completed A-Gradle

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,23 @@
+
+import static java.nio.CharBuffer.wrap;
+
 public class Duke {
+
+    private static String border = "____________________________________________________________";
+    private static String logo =
+              " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+
+    /**
+     * Program starts here.
+     */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println(wrap("What can I do for you?"));
+        System.out.println(border + "\n");
+
     }
 }
