fc96fbeaf419f0ad2142e0e15a5bdb5178325c26
Rusdi Haizim <rusdihaizim@gmail.com>
2019-08-21 18:12:38 +0800

    Completed Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 import static java.nio.CharBuffer.wrap;
 
@@ -15,9 +16,30 @@ public class Duke {
      * Program starts here.
      */
     public static void main(String[] args) {
-        System.out.println("Hello from\n" + logo);
+        System.out.println(border);
+        System.out.println("Hello! I'm Duke");
         System.out.println(wrap("What can I do for you?"));
         System.out.println(border + "\n");
 
+        handleCommand();
+    }
+
+    private static void handleCommand() {
+        Scanner sc = new Scanner(System.in);
+        level_1(sc);
+    }
+
+    private static void level_1(Scanner sc) {
+        String input = "";
+        while (sc.hasNext()) {
+            input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(border + "\n" + wrap("Bye. Hope to see you again soon!") + "\n" + border + "\n");
+                break;
+            }
+            else {
+                System.out.println(border + "\n" + wrap(input) + "\n" + border + "\n");
+            }
+        }
     }
 }
